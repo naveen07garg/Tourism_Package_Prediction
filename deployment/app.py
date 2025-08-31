32e0e29e4bc849e6a56517f1b19c9b9a3b6480e6
naveen07garg <naveen07garg@gmail.com>
2025-08-31 14:23:41 +0000

    Model data files upload to GitHub

--- deployment/app.py
+++ deployment/app.py
@@ -5,19 +5,23 @@ from huggingface_hub import hf_hub_download
 import joblib
 
 #=== Download and load the model ===
+#model_path = "hf://naveen07garg/Tourism-Package-Prediction/sales_prediction_model_v1.joblib"
 model_path = hf_hub_download(repo_id="naveen07garg/Tourism-Package-Prediction", filename="sales_prediction_model_v1.joblib")
 model = joblib.load(model_path)
+
+city_tier_map = {"Tier 1": 1, "Tier 2": 2, "Tier 3": 3}
 
 # naveen07garg/Tourism-Package-Prediction
 
 st.title("Visit With Us - Tour Prediction ")
 st.write("Fill the details to predict:")
 
-# Input form
+#==== Input form === 
 
 # Categorical (selectbox)
 typeof_contact = st.selectbox("Type of Contact", ["Self Enquiry", "Company Invited"])
-city_tier = st.selectbox("City Tier", [1, 2, 3])
+#city_tier = st.selectbox("City Tier", [1, 2, 3])
+city_tier = st.selectbox("City Tier", ["Tier 1", "Tier 2", "Tier 3"])
 occupation = st.selectbox("Occupation", ["Salaried", "Free Lancer", "Small Business", "Large Business"])
 gender = st.selectbox("Gender", ["Female", "Male", "Fe Male"])
 product_pitched = st.selectbox("Product Pitched", ["Deluxe", "Basic", "Standard", "Super Deluxe", "King"])
@@ -33,27 +37,28 @@ pitch_satisfaction_score = st.number_input("Pitch Satisfaction Score", min_value
 num_children_visiting = st.number_input("Number of Children Visiting", min_value=0, max_value=3, value=0)
 
 # Binary (selectbox)
-passport = st.selectbox("Has Passport", [0, 1])
-own_car = st.selectbox("Own Car", [0, 1])
-
+#passport = st.selectbox("Has Passport", [0, 1])
+#own_car = st.selectbox("Own Car", [0, 1])
+passport = st.selectbox("Has Passport", ["Yes", "No"])
+own_car = st.selectbox("Owns a Car", ["Yes", "No"])
 
 #=== Making a data frame ===
 input_data = pd.DataFrame([{
     "TypeofContact": typeof_contact,
-    "CityTier": city_tier,
+    "CityTier": city_tier_map[city_tier],
     "Occupation": occupation,
     "Gender": gender,
     "ProductPitched": product_pitched,
     "PreferredPropertyStar": preferred_property_star,
     "MaritalStatus": marital_status,
-    "Designation": designation
+    "Designation": designation,
     "NumberOfPersonVisiting": num_person_visiting,
     "NumberOfFollowups": num_followups,
     "NumberOfTrips": num_trips,
     "PitchSatisfactionScore": pitch_satisfaction_score,
     "NumberOfChildrenVisiting": num_children_visiting,
-    "Passport": passport,
-    "OwnCar": own_car,
+    "Passport": 1 if passport == "Yes" else 0,
+    "OwnCar": 1 if own_car == "Yes" else 0
 }])
 
 if st.button("Predict Sales Status"):
