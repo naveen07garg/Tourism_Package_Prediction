a113b4838ff5cdde67d801ab75caa58395448648
naveen07garg <naveen07garg@gmail.com>
2025-08-31 14:55:39 +0000

    Model data files upload to GitHub

--- deployment/app.py
+++ deployment/app.py
@@ -13,21 +13,21 @@ city_tier_map = {"Tier 1": 1, "Tier 2": 2, "Tier 3": 3}
 
 # naveen07garg/Tourism-Package-Prediction
 
-st.title("Visit With Us - Tour Prediction ")
-st.write("Fill the details to predict:")
+st.title("""Visit With Us""" + " - Tour Prediction ")
+st.write("Fill the customer details for to find if customer is likely to Opt the package:")
 
 #==== Input form === 
 
 # Categorical (selectbox)
 typeof_contact = st.selectbox("Type of Contact", ["Self Enquiry", "Company Invited"])
 #city_tier = st.selectbox("City Tier", [1, 2, 3])
-city_tier = st.selectbox("City Tier", ["Tier 1", "Tier 2", "Tier 3"])
-occupation = st.selectbox("Occupation", ["Salaried", "Free Lancer", "Small Business", "Large Business"])
+city_tier = st.selectbox("City Tier [Basis Tier 1 - Metro City | Tier 2 - B Grade City | Tier 3 - Small City]", ["Tier 1", "Tier 2", "Tier 3"])
+occupation = st.selectbox("Occupation of Customer", ["Salaried", "Free Lancer", "Small Business", "Large Business"])
 gender = st.selectbox("Gender", ["Female", "Male", "Fe Male"])
 product_pitched = st.selectbox("Product Pitched", ["Deluxe", "Basic", "Standard", "Super Deluxe", "King"])
 preferred_property_star = st.selectbox("Preferred Property Star", [3, 4, 5])
 marital_status = st.selectbox("Marital Status", ["Single", "Divorced", "Married", "Unmarried"])
-designation = st.selectbox("Designation", ["Manager", "Executive", "Senior Manager", "AVP", "VP"])
+designation = st.selectbox("Designation in the Organisation", ["Manager", "Executive", "Senior Manager", "AVP", "VP"])
 
 # Numeric (number_input)
 num_person_visiting = st.number_input("Number of Persons Visiting", min_value=1, max_value=5, value=1)
@@ -35,6 +35,10 @@ num_followups = st.number_input("Number of Followups", min_value=1, max_value=6,
 num_trips = st.number_input("Number of Trips", min_value=1, max_value=22, value=1)
 pitch_satisfaction_score = st.number_input("Pitch Satisfaction Score", min_value=1, max_value=5, value=3)
 num_children_visiting = st.number_input("Number of Children Visiting", min_value=0, max_value=3, value=0)
+
+monthly_income = st.number_input("Monthly Income of customer", min_value=100, max_value=200000, value=0)
+duration_of_pitch = st.number_input("Duration of customer pich by Agent", min_value=0, max_value=150, value=30)
+age = st.number_input("Age of Customer", min_value=18, max_value=120, value=18)
 
 # Binary (selectbox)
 #passport = st.selectbox("Has Passport", [0, 1])
@@ -57,6 +61,9 @@ input_data = pd.DataFrame([{
     "NumberOfTrips": num_trips,
     "PitchSatisfactionScore": pitch_satisfaction_score,
     "NumberOfChildrenVisiting": num_children_visiting,
+    'MonthlyIncome':monthly_income,
+    'DurationOfPitch':duration_of_pitch,
+    'Age':age,
     "Passport": 1 if passport == "Yes" else 0,
     "OwnCar": 1 if own_car == "Yes" else 0
 }])
